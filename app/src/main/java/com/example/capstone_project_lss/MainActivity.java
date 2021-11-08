aa728caf1c0e57dd7a61a1ae7db0bd85db31321a
ClydeBSIS-HUB <eespinosa@spcf.edu.ph>
2021-11-08 15:27:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/capstone_project_lss/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.capstone_project_lss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnlogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnlogin = (Button) findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+
+    }
+
+    public void openLogin() {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/capstone_project_lss/Signup.java
@@ -0,0 +1,14 @@
+package com.example.capstone_project_lss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
